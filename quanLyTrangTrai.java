541acd54c3dae6ab2df1461fb0412bcdc9851bec
trung <tuantrung0507@gmail.com>
2024-10-07 16:35:19 +0700

    first commit

---
+++ quanLyTrangTrai.java
@@ -0,0 +1,49 @@
+package bai71;
+
+public class quanLyTrangTrai {
+    public class dongVat {
+        public boolean khaNangDiBo() {
+        }
+        public boolean khaNangDiBoi() {
+        }
+    }
+    public class lon extends dongVat {
+        public boolean khaNangDiBo() {
+            return true;
+        }
+        public boolean khaNangDiBoi() {
+            return false;
+        }
+    }
+    public class vit extends dongVat {
+        public boolean khaNangDiBo() {
+            return true;
+        }
+        public boolean khaNangDiBoi() {
+            return true;
+        }
+    }
+
+    public class ca extends dongVat {
+        public boolean khaNangDiBo() {
+            return false;
+        }
+        public boolean khaNangDiBoi() {
+            return true;
+        }
+    }
+
+    static public void main (String [] args) {
+        quanLyTrangTrai qltt = new quanLyTrangTrai();
+        dongVat dv = qltt.new dongVat();
+        lon l = qltt.new lon();
+        vit v = qltt.new vit();
+        ca c = qltt.new ca();
+        System.out.println("lon co kha nang di bo: " + l.khaNangDiBo());
+        System.out.println("lon co kha nang di boi: " + l.khaNangDiBoi());
+        System.out.println("vit co kha nang di bo: " + v.khaNangDiBo());
+        System.out.println("vit co kha nang di boi: " + v.khaNangDiBoi());
+        System.out.println("ca co kha nang di bo: " + c.khaNangDiBo());
+        System.out.println("ca co kha nang di boi: " + c.khaNangDiBoi());
+    }
+}
